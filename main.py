5473c7a642d9f61f982c35fdba8a4524b73b3f31
Abhijeet Srivastava <abhijeet181995@gmail.com>
2020-10-27 13:20:50 +0530

    Added keyword visualization

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import re
 import pandas as pd
 from matplotlib import pyplot as plt
+from collections import Counter
 
 with open("Bibtex.txt",'r') as rf:
     data = rf.read()
 
 year=[]
-
-
+keyword=[]
 biblist = [x for x in data.split('@INPROCEEDINGS')]
 dic={}
 for bib in biblist:
     if re.search("year={",bib) is not None:
         year.append(int(bib[re.search("year={",bib).span()[1]:re.search("year={",bib).span()[1]+4]))
+        keyword+=bib[re.search("keywords={.*}",bib).span()[0]+10:re.search("keywords={.*}",bib).span()[1]-1].split(';')
+
+
+# print(([Counter(x.split(';')) for x in keyword]))
+counts = Counter(keyword)
+new = plt.figure(2)
+plt.bar(counts.keys(),counts.values())
+plt.show()
 
 df = pd.DataFrame({'year':year})
-
-# df.hist()
-plt.style.use('fivethirtyeight') 
+plt.style.use('fivethirtyeight')
 df.hist()
 plt.show()
+
+
+
 
 
