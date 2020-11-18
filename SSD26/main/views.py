7c76838c7ce7e171c693b1525d4ccead308df65c
rishi1tripathi <rishitripathi@gmail.com>
2020-11-19 03:26:15 +0530

    Broken code into functions added two pie plot author count top 5 keyword count

--- SSD26/main/views.py
+++ SSD26/main/views.py
@@ -35,20 +35,7 @@ def upload_file(request):
     return render(request, 'upload.html', {'form': form})
 
 
-
-def graph(request):
-    filelist = os.listdir('temp') 
-    df_list = [  ]
-    for file in filelist:
-        f = open('temp/'+file, encoding="utf-8")
-        bib_database = bibtexparser.load(f)
-        df_ENTRY = pd.DataFrame(bib_database.entries)
-        df_list.append(df_ENTRY)
-        f.close()
-        os.remove('temp/'+file)
-    df = pd.concat(df_list,ignore_index=True)
-
-    df.pages
+def plotPagesCount(df):
     maxValue=0
     pages_count = {}
     for i in df.pages:
@@ -57,7 +44,6 @@ def graph(request):
             maxValue= max(maxValue,y-x)
         except:
             continue
-
     if(maxValue>100):
         divider=10
     elif(maxValue>50):
@@ -77,18 +63,16 @@ def graph(request):
                 pages_count[pageWidthCluster] = pages_count.get(pageWidthCluster,0)+1
         except:
             continue
-
     od = OrderedDict(sorted(pages_count.items()))
 
-    fig1 = plt.figure(figsize = (10, 5)) 
+    fig = plt.figure(figsize = (10, 5)) 
     plt.bar(list( od.keys() ), list( od.values() ), color ='royalblue',  width = 0.4) 
     plt.xlabel("Pages") 
     plt.ylabel("Count") 
     plt.title("Number of pages in different papers") 
-    
-    g = mpld3.fig_to_html(fig1)
+    return mpld3.fig_to_html(fig)
 
-
+def plotMonthYearAnalysis(df):
     month_to_paper = dict()
     monthsMapper = {'jan':'Jan','january':'Jan','feb':'Feb','february':'Feb','march':'Mar','mar':'Mar','april':'Apr','apr':'Apr','may':'May','june':'Jun','jun':'Jun','july':'Jul','jul':'Jul','aug':'Aug','august':'Aug','sep':'Sep','september':'Sep','oct':'Oct','october':'Oct','november':'Nov','nov':'Nov','december':'Dec','dec':'Dec'}
 
@@ -97,29 +81,23 @@ def graph(request):
             key=monthsMapper[i.lower()]
             month_to_paper[key] = month_to_paper.get(key,0)+1
 
-    fig2 = plt.figure(figsize = (10, 5)) 
-
-
+   
     months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-
     month_num = dict(zip(months, range(12)))
-
     def compareMonth(month):
         return month_num[month]
     sorted_month=dict(sorted(month_to_paper.items(), key=lambda x: compareMonth(x[0])))
+    fig1 = plt.figure(figsize = (10, 5)) 
     plt.bar(list( sorted_month.keys() ), list( sorted_month.values() ), color ='crimson',  width = 0.4) 
-
     plt.xlabel("Month") 
     plt.ylabel("Count") 
     plt.title("Number of papers in different months")
-
-    g += mpld3.fig_to_html(fig2)
 
     year_to_paper = {}
     for i in df.year.dropna():
         year_to_paper[i] = year_to_paper.get(i,0)+1
 
-    fig3 = plt.figure(figsize = (10, 5))
+    fig2 = plt.figure(figsize = (10, 5))
     sorted_year = OrderedDict(sorted(year_to_paper.items()))
 
     plt.bar(list( sorted_year.keys() ), list( sorted_year.values() ), color ='crimson',  width = 0.4) 
@@ -127,8 +105,6 @@ def graph(request):
     plt.xlabel("Year") 
     plt.ylabel("Count") 
     plt.title("Number of papers published in different years") 
-
-    g += mpld3.fig_to_html(fig3)
 
 
     
@@ -146,23 +122,17 @@ def graph(request):
                 continue
     sorted_year_to_month_papers=OrderedDict(sorted(year_to_month_papers.items(), key=lambda x: x[0]))
     df2 = pd.DataFrame(sorted_year_to_month_papers)
-
     ax = df2.plot.bar(figsize=(12,8),width = 0.4,stacked=True)
     ax.set_xlabel("Month")
     ax.set_ylabel("No.of Papers")
-    fig4 = ax.get_figure()
-
-    g += mpld3.fig_to_html(fig4)
-
+    fig3 = ax.get_figure()
     ax = df2.T.plot.bar(figsize=(12,8),width = 0.4,stacked=True)
     ax.set_xlabel("Year")
     ax.set_ylabel("No.of Papers")
-    fig5 = ax.get_figure()
+    fig4 = ax.get_figure()
+    return mpld3.fig_to_html(fig1) +mpld3.fig_to_html(fig2)+mpld3.fig_to_html(fig3)+mpld3.fig_to_html(fig4)
 
-    g += mpld3.fig_to_html(fig5)
-
-
-
+def plotHistoricalTrend(df):
     keyWordDict={}
     for index, row in df.iterrows():
         for keyWord in row['keywords'].split(';'):
@@ -185,47 +155,69 @@ def graph(request):
 
 
     sortedKeyWord = OrderedDict(sorted(keyWordDict.items(), key=lambda kv: kv[1]['count'], reverse=True))
-
-    fig6 = plt.figure(figsize = (12, 8))
+    fig = plt.figure(figsize = (12, 8))
+    totalCountDict={}
     for index, (key, value) in enumerate(sortedKeyWord.items()):
         # print(index, key, value)
         temp = OrderedDict(sorted(value['yearsMap'].items(), key=lambda kv: kv[0]))
+        totalCountDict[key]=value['count']
         plt.plot(list(temp.keys()), list(temp.values()), label = key, marker='o')
         if index==5:
             break
     plt.xlabel('Years')
     plt.ylabel('Count')
-    plt.title('Keyword historical trend ')
+    plt.title('Top 5 Keyword historical trend ')
     plt.legend()
+    sortedCountDict = OrderedDict(sorted(totalCountDict.items(), key=lambda kv: kv[1]))
+    fig2 = plt.figure(figsize = (12, 8))
+    plt.pie(sortedCountDict.values()[:5], labels = sortedCountDict.keys()[:5]) 
+    plt.title('Top 5 keywords ')
+    return mpld3.fig_to_html(fig)+mpld3.fig_to_html(fig2)
 
-    g += mpld3.fig_to_html(fig6)
 
-
+def plotWordCloud(df):
     vocab_w_freq = []
-
     for i in df.keywords:
         r = i.split(';')
         vocab_w_freq.extend(r)
     vocab = set( vocab_w_freq )
-
-
-
     stopwords = set(STOPWORDS) 
     vocab_w_freq_str = " ".join(vocab_w_freq)
-
-
-    
     wordcloud = WordCloud(width = 800, height = 800, background_color ='white', stopwords = stopwords, min_font_size = 10).generate(vocab_w_freq_str) 
-    
-    # plot the WordCloud image
-    fig5 = plt.figure(figsize = (8, 8), facecolor = None) 
+    fig = plt.figure(figsize = (8, 8), facecolor = None) 
     plt.imshow(wordcloud) 
     plt.axis("off") 
     plt.tight_layout(pad = 0) 
+    return mpld3.fig_to_html(fig)
 
-    g += mpld3.fig_to_html(fig5)
+def plotAuthor(df):
+    author_count = {}
+    for authors in df.author.dropna():
+        for author in authors.split('and'):
+            author_count[author] = author_count.get(author,0)+1
+    fig = plt.figure(figsize = (12, 8))
+    sorted_author_count=OrderedDict(sorted(author_count.items(), key=lambda x: x[1]),reverse=True)
+    plt.pie(sorted_author_count.values()[:5], labels = sorted_author_count.keys()[:5]) 
+    plt.title('Most publishing author ')
+    return mpld3.fig_to_html(fig)
 
 
-    
+def graph(request):
+    filelist = os.listdir('temp') 
+    df_list = [  ]
+    for file in filelist:
+        f = open('temp/'+file, encoding="utf-8")
+        bib_database = bibtexparser.load(f)
+        df_ENTRY = pd.DataFrame(bib_database.entries)
+        df_list.append(df_ENTRY)
+        f.close()
+        os.remove('temp/'+file)
+    df = pd.concat(df_list,ignore_index=True)
+
+    g=plotPagesCount(df)
+    g+=plotMonthYearAnalysis(df)
+    g+=plotHistoricalTrend(df)
+    g+=plotWordCloud(df)
+    g+=plotAuthor(df)
     return render(request,'graph.html',{'graph':g})
 
